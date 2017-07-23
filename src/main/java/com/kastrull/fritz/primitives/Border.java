968ea2e1bd9b7ecec9046ab7c46307541b89b984
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2017-07-23 09:49:40 +0200

    Add primitives.Border

---
+++ src/main/java/com/kastrull/fritz/primitives/Border.java
@@ -0,0 +1,20 @@
+package com.kastrull.fritz.primitives;
+
+public class Border {
+
+	public static final boolean BY_X = true;
+	public static final boolean BY_Y = false;
+
+	public final double at;
+	public final boolean byX;
+
+	public Border(double at, boolean byX) {
+		this.at = at;
+		this.byX = byX;
+	}
+
+	public static Border b(double at, boolean byX) {
+		// TODO Auto-generated method stub
+		return new Border(at, byX);
+	}
+}
---
+++ src/test/java/com/kastrull/fritz/primitives/BorderTest.java
@@ -0,0 +1,25 @@
+package com.kastrull.fritz.primitives;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class BorderTest implements WithQtAndPrimitives {
+
+	@Test
+	public void canCreate() {
+		qt()
+			.forAll(
+				doublesToInf(),
+				booleans().all())
+			.checkAssert((at, byX) -> {
+				Border b = Border.b(at, byX);
+
+				assertNotNull(b);
+				assertEquals(at, b.at, 0.0);
+				assertEquals(byX, b.byX);
+			});
+	}
+
+}
