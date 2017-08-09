367d447b42f4ce2260f9c39937fa2e28dc8d77be
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2017-08-09 21:13:01 +0200

    Assert generated state particles avg velocity

--- src/main/java/com/kastrull/fritz/sim/SimStateGenerator.java
+++ src/main/java/com/kastrull/fritz/sim/SimStateGenerator.java
@@ -1,14 +1,18 @@
 package com.kastrull.fritz.sim;
 
-import java.util.ArrayList;
+import static com.codepoetics.protonpack.StreamUtils.zip;
+
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.kastrull.fritz.primitives.Coord;
 import com.kastrull.fritz.primitives.Particle;
 
 public final class SimStateGenerator {
 
+	private static final double ZERO = 0.0;
 	private static final double START_TIME = 0.0;
 
 	public static SimStateGenerator create(SimSetup setup) {
@@ -20,7 +24,25 @@ public final class SimStateGenerator {
 
 	public SimStateGenerator(SimSetup setup) {
 		this.setup = setup;
-		this.r = new Random(setup.rndSeed);
+
+		r = new Random(setup.rndSeed);
+	}
+
+	private Stream<Particle> particleStream() {
+		Stream<Double> posX = setup.size.x == 0 ? Stream.generate(() -> 0.0) : r.doubles(ZERO, setup.size.x).boxed();
+		Stream<Double> posY = setup.size.y == 0 ? Stream.generate(() -> 0.0) : r.doubles(ZERO, setup.size.y).boxed();
+		Stream<Coord> pos = zip(posX, posY, Coord::c);
+
+		Stream<Coord> vel;
+		double max = setup.maxSpeed;
+		if (max == 0.0) {
+			vel = Stream.generate(() -> Coord.ZERO);
+		} else {
+			Stream<Double> velX = r.doubles(-max, max + Double.MIN_NORMAL).boxed();
+			Stream<Double> velY = r.doubles(-max, max + Double.MIN_NORMAL).boxed();
+			vel = zip(velX, velY, Coord::c);
+		}
+		return zip(pos, vel, Particle::p);
 	}
 
 	public SimState generateState() {
@@ -28,23 +50,10 @@ public final class SimStateGenerator {
 			.createWithWalls(setup.size.x, setup.size.y)
 			.currentTime(START_TIME)
 			.targetTime(setup.simTime)
-			.particles(randomParticles(setup.particleCount));
+			.particles(particleListOfLength(setup.particleCount));
 	}
 
-	private final Particle randomParticle() {
-		return Particle.p(randomCoord(), randomCoord());
-	}
-
-	private final Coord randomCoord() {
-		return Coord.c(r.nextDouble(), r.nextDouble());
-	}
-
-	private List<Particle> randomParticles(int count) {
-		List<Particle> ps = new ArrayList<>(count);
-
-		for (int i = 0; i < count; i++)
-			ps.add(randomParticle());
-
-		return ps;
+	private List<Particle> particleListOfLength(int length) {
+		return particleStream().limit(length).collect(Collectors.toList());
 	}
 }
--- src/test/java/com/kastrull/fritz/sim/SimStateGeneratorTest.java
+++ src/test/java/com/kastrull/fritz/sim/SimStateGeneratorTest.java
@@ -5,14 +5,19 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.junit.Test;
 import org.quicktheories.quicktheories.WithQuickTheories;
 
 import com.kastrull.fritz.primitives.Border;
 import com.kastrull.fritz.primitives.Particle;
+import com.kastrull.fritz.primitives.WithAssert;
 
-public class SimStateGeneratorTest implements WithQuickTheories, WithSimSources {
+public class SimStateGeneratorTest implements WithQuickTheories, WithSimSources, WithAssert {
+
+	private static final double FIFTY_PERCENT = 0.30;
 
 	@Test
 	@SuppressWarnings("unused")
@@ -87,10 +92,42 @@ public class SimStateGeneratorTest implements WithQuickTheories, WithSimSources 
 	}
 
 	@Test
+	public void particleVelocity() {
+		qt()
+			.forAll(
+				simSetups())
+			.assuming(setup -> setup.particleCount > 20)
+			.checkAssert(
+				setup -> {
+					double expectedAvgSpeed = setup.maxSpeed / 2;
+
+					SimState state = genState(setup);
+
+					double avgSpeedX = avg(state, p -> Math.abs(p.vel.x));
+					double avgSpeedY = avg(state, p -> Math.abs(p.vel.y));
+
+					assertEquals(expectedAvgSpeed, avgSpeedX, expectedAvgSpeed * FIFTY_PERCENT);
+					assertEquals(expectedAvgSpeed, avgSpeedY, expectedAvgSpeed * FIFTY_PERCENT);
+				});
+	}
+
+	private double avg(SimState state, Function<Particle, Double> mapper) {
+
+		double actualAccSpeedX = state
+			.particles()
+			.stream()
+			.map(mapper)
+			.reduce(0.0, (a, b) -> a + b);
+
+		return actualAccSpeedX / state.particles().size();
+	}
+
+	@Test
 	public void sameSetupGeneratesSameStatesInDifferentGenerators() {
 		qt()
 			.forAll(
 				simSetups())
+			.assuming(lowParticleCount())
 			.checkAssert(
 				setup -> {
 					SimStateGenerator g1 = SimStateGenerator.create(setup);
@@ -98,8 +135,11 @@ public class SimStateGeneratorTest implements WithQuickTheories, WithSimSources 
 					assertEquals(g1.generateState(), g2.generateState());
 					assertEquals(g1.generateState(), g2.generateState());
 					assertEquals(g1.generateState(), g2.generateState());
-					assertEquals(g1.generateState(), g2.generateState());
 				});
+	}
+
+	private Predicate<SimSetup> lowParticleCount() {
+		return setup -> setup.particleCount < 200;
 	}
 
 	@Test
@@ -108,14 +148,18 @@ public class SimStateGeneratorTest implements WithQuickTheories, WithSimSources 
 			.forAll(
 				simSetups())
 			.assuming(setup -> setup.particleCount > 0)
+			.assuming(lowParticleCount())
 			.checkAssert(
 				setup -> {
 					SimStateGenerator g = SimStateGenerator.create(setup);
 
-					assertNotEquals(g.generateState(), g.generateState());
-					assertNotEquals(g.generateState(), g.generateState());
-					assertNotEquals(g.generateState(), g.generateState());
-					assertNotEquals(g.generateState(), g.generateState());
+					SimState s1 = g.generateState();
+					SimState s2 = g.generateState();
+					SimState s3 = g.generateState();
+					SimState s4 = g.generateState();
+					assertNotEquals(s1, s2);
+					assertNotEquals(s2, s3);
+					assertNotEquals(s3, s4);
 				});
 	}
 
