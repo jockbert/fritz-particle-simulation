08c4169baca97dd1aa1e2fefd4afcac0a58d0f71
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2018-09-19 09:03:10 +0200

    Remove EventAndAction from Simulator

--- src/main/java/com/kastrull/fritz/sim/EngineDrivenSimulator.java
+++ src/main/java/com/kastrull/fritz/sim/EngineDrivenSimulator.java
@@ -17,13 +17,9 @@ import com.kastrull.fritz.primitives.WallInteraction;
 public class EngineDrivenSimulator implements Simulator {
 
 	private Physics physics;
-
-	private EventEngine<SimParticle, EventAndAction> engine = new BasicEventEngine<>();
-
+	private EventEngine<SimParticle, Event> engine = new BasicEventEngine<>();
 	private List<SimParticle> particles;
-
 	private SimState initialState;
-
 	private double totalMomentum = 0;
 
 	public EngineDrivenSimulator(LinearPhysics linearPhysics) {
@@ -35,55 +31,31 @@ public class EngineDrivenSimulator implements Simulator {
 		initialState = state;
 
 		initParticles(state);
-
 		initWalls(state);
 
 		addSimulationEnd(state);
-
 		addAllWallHits(state);
-
 		addAllParticleHits(state);
 
 		Optional<Double> endTime = Optional.empty();
 
-		simloop: for (Outcome<SimParticle, EventAndAction> oc : engine) {
+		simloop: for (Outcome<SimParticle, Event> oc : engine) {
 
 			boolean isAfterSimEndTime = endTime.isPresent() && endTime.get() < oc.time();
 			if (isAfterSimEndTime)
 				break simloop;
 
-			// Result has side effects, so must be executed after
-			// breaking simulation loop.
-			EventAndAction ena = oc.result();
-			Event event = ena.event;
-			ena.action.run();
-
-			switch (event) {
-
-			case SIM_END:
-				// Continue simulation to consume all
-				// events happening at the same time.
-				endTime = Optional.of(Double.valueOf(oc.time()));
-				break;
-
-			case WALL_HIT:
-			case PARTICLE_HIT:
-
-				oc
-					.involves()
-					.forEach(p -> {
-						addParticleHit(p, oc.time());
-						addWallHit(p, oc.time());
-					});
-
-				break;
-
-			default:
-				throw new RuntimeException("Can't handle event type " + event);
-			}
+			Event event = oc.result();
+			totalMomentum += event.interact(oc.time());
+			endTime = event.isAtEnd() ? Optional.of(oc.time()) : endTime;
 		}
 
 		return endState(state);
+	}
+
+	private void addParticleEventsToEngine(SimParticle p, double currentTime) {
+		addParticleHit(p, currentTime);
+		addWallHit(p, currentTime);
 	}
 
 	private void initWalls(SimState state) {
@@ -108,32 +80,10 @@ public class EngineDrivenSimulator implements Simulator {
 			.filter(otherP -> otherP != p)
 			.forEach(otherP -> physics
 				.collisionTime(particle, otherP.getAtTime(currentTime))
-				.ifPresent(hitTime -> engine
-					.addEvent(
-						currentTime + hitTime,
-						ena(
-							Event.PARTICLE_HIT,
-							particleHitAction(p, otherP, currentTime + hitTime)),
-						p, otherP)));
-	}
-
-	private EventAndAction ena(Event e, Runnable a) {
-		return new EventAndAction(e, a);
-	}
-
-	private Runnable particleHitAction(
-			SimParticle p1,
-			SimParticle p2,
-			double particleHitTime) {
-
-		return () -> {
-			Interaction i = physics.interact(
-				p1.getAtTime(particleHitTime),
-				p2.getAtTime(particleHitTime));
-
-			p1.modify(i.p1, particleHitTime);
-			p2.modify(i.p2, particleHitTime);
-		};
+				.ifPresent(hitTime -> engine.addEvent(
+					currentTime + hitTime,
+					new ParticleHit(p, otherP),
+					p, otherP)));
 	}
 
 	private void addAllWallHits(SimState state) {
@@ -148,33 +98,14 @@ public class EngineDrivenSimulator implements Simulator {
 			.stream()
 			.forEach(wall -> physics
 				.collisionTimeWall(particle, wall)
-				.ifPresent(hitTime -> engine
-					.addEvent(
-						currentTime + hitTime,
-						ena(Event.WALL_HIT,
-							wallHitAction(p, wall, currentTime + hitTime)),
-						p)));
-	}
-
-	private Runnable wallHitAction(
-			SimParticle p,
-			Border wall,
-			double wallHitTime) {
-
-		return () -> {
-			WallInteraction wi = physics.interactWall(p.getAtTime(wallHitTime), wall);
-			totalMomentum += wi.wallMomentum.distance();
-			p.modify(wi.p, wallHitTime);
-		};
+				.ifPresent(hitTime -> engine.addEvent(
+					currentTime + hitTime,
+					new WallHit(p, wall),
+					p)));
 	}
 
 	private void addSimulationEnd(SimState state) {
-		engine.addEvent(
-			state.targetTime(),
-			ena(
-				Event.SIM_END,
-				() -> {
-					/* do nothing */ }));
+		engine.addEvent(state.targetTime(), new SimEnd());
 	}
 
 	private SimState endState(SimState state) {
@@ -185,18 +116,67 @@ public class EngineDrivenSimulator implements Simulator {
 				.map(p -> p.getAtTime(state.targetTime()))
 				.collect(Collectors.toList()));
 	}
-}
 
-enum Event {
-	WALL_HIT, SIM_END, PARTICLE_HIT
-}
+	abstract class Event {
 
-class EventAndAction {
-	public final Event event;
-	public final Runnable action;
+		/** Modify particles and return wall momentum. */
+		double interact(double time) {
+			// do nothing
+			return 0;
+		}
 
-	EventAndAction(Event e, Runnable a) {
-		this.event = e;
-		this.action = a;
+		boolean isAtEnd() {
+			// do not end
+			return false;
+		}
+	}
+
+	final class SimEnd extends Event {
+		@Override
+		boolean isAtEnd() {
+			return true;
+		}
+	}
+
+	final class WallHit extends Event {
+		private SimParticle p;
+		private Border border;
+
+		WallHit(SimParticle p, Border border) {
+			this.p = p;
+			this.border = border;
+		}
+
+		@Override
+		double interact(double time) {
+			WallInteraction wi = physics.interactWall(p.getAtTime(time), border);
+			p.modify(wi.p, time);
+
+			addParticleEventsToEngine(p, time);
+			return wi.wallMomentum.distance();
+		}
+	}
+
+	final class ParticleHit extends Event {
+		private SimParticle p1, p2;
+
+		ParticleHit(SimParticle p1, SimParticle p2) {
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+
+		@Override
+		double interact(double time) {
+			Interaction i = physics.interact(
+				p1.getAtTime(time),
+				p2.getAtTime(time));
+
+			p1.modify(i.p1, time);
+			p2.modify(i.p2, time);
+
+			addParticleEventsToEngine(p1, time);
+			addParticleEventsToEngine(p2, time);
+			return 0;
+		}
 	}
 }
